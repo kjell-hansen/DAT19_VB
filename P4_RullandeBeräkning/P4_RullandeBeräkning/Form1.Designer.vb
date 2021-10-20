c51c01aba20696ec529f518422e7ee987325f9e9
Kjell Hansen (S1-KJHA-9205) <kjell.hansen@gymnasium.ax>
2021-10-20 09:46:08 +0300

    Lagt till P4_RullandeBräkning

---
+++ P4_RullandeBeräkning/P4_RullandeBeräkning/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtTal = New System.Windows.Forms.TextBox()
+        Me.btnLaggTill = New System.Windows.Forms.Button()
+        Me.lstTal = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblSumma = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblMedel = New System.Windows.Forms.Label()
+        Me.btnBorjaOm = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtTal
+        '
+        Me.txtTal.Location = New System.Drawing.Point(61, 39)
+        Me.txtTal.Name = "txtTal"
+        Me.txtTal.Size = New System.Drawing.Size(144, 27)
+        Me.txtTal.TabIndex = 0
+        '
+        'btnLaggTill
+        '
+        Me.btnLaggTill.Location = New System.Drawing.Point(296, 38)
+        Me.btnLaggTill.Name = "btnLaggTill"
+        Me.btnLaggTill.Size = New System.Drawing.Size(102, 37)
+        Me.btnLaggTill.TabIndex = 1
+        Me.btnLaggTill.Text = "Lägg till"
+        Me.btnLaggTill.UseVisualStyleBackColor = True
+        '
+        'lstTal
+        '
+        Me.lstTal.FormattingEnabled = True
+        Me.lstTal.ItemHeight = 20
+        Me.lstTal.Location = New System.Drawing.Point(509, 45)
+        Me.lstTal.Name = "lstTal"
+        Me.lstTal.Size = New System.Drawing.Size(214, 204)
+        Me.lstTal.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(80, 119)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Summa"
+        '
+        'lblSumma
+        '
+        Me.lblSumma.AutoSize = True
+        Me.lblSumma.Location = New System.Drawing.Point(166, 120)
+        Me.lblSumma.Name = "lblSumma"
+        Me.lblSumma.Size = New System.Drawing.Size(78, 20)
+        Me.lblSumma.TabIndex = 4
+        Me.lblSumma.Text = "lblSUmma"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(80, 157)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Medel"
+        '
+        'lblMedel
+        '
+        Me.lblMedel.AutoSize = True
+        Me.lblMedel.Location = New System.Drawing.Point(166, 158)
+        Me.lblMedel.Name = "lblMedel"
+        Me.lblMedel.Size = New System.Drawing.Size(68, 20)
+        Me.lblMedel.TabIndex = 4
+        Me.lblMedel.Text = "lblMedel"
+        '
+        'btnBorjaOm
+        '
+        Me.btnBorjaOm.Location = New System.Drawing.Point(300, 98)
+        Me.btnBorjaOm.Name = "btnBorjaOm"
+        Me.btnBorjaOm.Size = New System.Drawing.Size(97, 40)
+        Me.btnBorjaOm.TabIndex = 5
+        Me.btnBorjaOm.Text = "Börja om"
+        Me.btnBorjaOm.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnLaggTill
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnBorjaOm
+        Me.ClientSize = New System.Drawing.Size(800, 279)
+        Me.Controls.Add(Me.btnBorjaOm)
+        Me.Controls.Add(Me.lblMedel)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblSumma)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstTal)
+        Me.Controls.Add(Me.btnLaggTill)
+        Me.Controls.Add(Me.txtTal)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtTal As TextBox
+    Friend WithEvents btnLaggTill As Button
+    Friend WithEvents lstTal As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblSumma As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblMedel As Label
+    Friend WithEvents btnBorjaOm As Button
+End Class
---
+++ P4_RullandeBeräkning/P4_RullandeBeräkning/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+    Private Sub txtTal_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtTal.KeyPress
+        ' Tillåt siffror, backspace och .
+        If (e.KeyChar < "0"c Or e.KeyChar > "9"c) And e.KeyChar <> vbBack And e.KeyChar <> "."c Then
+            e.Handled = True
+        End If
+
+        ' Finns det redan en . strunta i nästa
+        If e.KeyChar = "."c And InStr(sender.text, ".") > 0 Then
+            e.Handled = True
+        End If
+
+        ' Hantera minustecken
+        If e.KeyChar = "-"c Then
+            If sender.Text.StartsWith("-"c) Then
+                sender.Text = Mid(sender.Text, 2)
+            Else
+                sender.Text = "-" & sender.Text
+            End If
+            sender.select(sender.text.length, 0)
+        End If
+    End Sub
+
+    Private Sub btnLaggTill_Click(sender As Object, e As EventArgs) Handles btnLaggTill.Click
+        ' Hoppa ut om talet är inkorrekt
+        If txtTal.Text = "" Or txtTal.Text = "." Or txtTal.Text = "-" Then
+            Exit Sub
+        End If
+
+        ' Lägg till talet från rutan
+        lstTal.Items.Add(txtTal.Text)
+
+        ' Rensa textrutan
+        txtTal.Text = ""
+
+        ' Räkna statistik
+        Statistik()
+    End Sub
+
+    Private Sub btnBorjaOm_Click(sender As Object, e As EventArgs) Handles btnBorjaOm.Click
+        ' Ta bort allt i listrutan
+        lstTal.Items.Clear()
+
+        ' Ta bort summa och medel
+        lblMedel.Text = ""
+        lblSumma.Text = ""
+
+        ' Ta bort text i textrutan och sätt fokus dit
+        txtTal.Text = ""
+        txtTal.Focus()
+
+    End Sub
+
+    Private Sub lstTal_KeyDown(sender As Object, e As KeyEventArgs) Handles lstTal.KeyDown
+        If e.KeyCode = Keys.Delete And lstTal.SelectedIndex > -1 Then
+            lstTal.Items.RemoveAt(lstTal.SelectedIndex)
+            Statistik()
+        End If
+    End Sub
+
+    Private Sub Statistik()
+        ' Summera alla tal i listan
+        Dim Summa As Double
+        For i = 0 To lstTal.Items.Count - 1
+            Summa = Summa + Val(lstTal.Items(i))
+        Next
+
+        lblSumma.Text = Summa.ToString
+
+        ' Räkna medel för alla tal i listan
+        lblMedel.Text = (Summa / lstTal.Items.Count).ToString
+
+    End Sub
+End Class
