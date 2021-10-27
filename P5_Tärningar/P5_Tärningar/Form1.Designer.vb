8c7f0113c2b098529b7b39c1c7ec1d59c4aeea0c
Kjell Hansen (S1-KJHA-9205) <kjell.hansen@gymnasium.ax>
2021-10-27 15:44:13 +0300

    Gjort om kontroller till arrayer och snyggat till koden

--- P5_Tärningar/P5_Tärningar/Form1.Designer.vb
+++ P5_Tärningar/P5_Tärningar/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmTärningar
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -23,12 +23,15 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmTärningar))
         Me.btnSla = New System.Windows.Forms.Button()
         Me.picTarning1 = New System.Windows.Forms.PictureBox()
         Me.picTarning2 = New System.Windows.Forms.PictureBox()
         Me.picTarning3 = New System.Windows.Forms.PictureBox()
         Me.imlTarningar = New System.Windows.Forms.ImageList(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
         CType(Me.picTarning1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picTarning2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picTarning3, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -36,16 +39,18 @@ Partial Class Form1
         '
         'btnSla
         '
-        Me.btnSla.Location = New System.Drawing.Point(164, 159)
+        Me.btnSla.Location = New System.Drawing.Point(144, 137)
+        Me.btnSla.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.btnSla.Name = "btnSla"
-        Me.btnSla.Size = New System.Drawing.Size(113, 60)
+        Me.btnSla.Size = New System.Drawing.Size(99, 45)
         Me.btnSla.TabIndex = 0
         Me.btnSla.Text = "Slå!"
         Me.btnSla.UseVisualStyleBackColor = True
         '
         'picTarning1
         '
-        Me.picTarning1.Location = New System.Drawing.Point(82, 53)
+        Me.picTarning1.Location = New System.Drawing.Point(72, 40)
+        Me.picTarning1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.picTarning1.Name = "picTarning1"
         Me.picTarning1.Size = New System.Drawing.Size(64, 64)
         Me.picTarning1.TabIndex = 1
@@ -53,7 +58,8 @@ Partial Class Form1
         '
         'picTarning2
         '
-        Me.picTarning2.Location = New System.Drawing.Point(185, 53)
+        Me.picTarning2.Location = New System.Drawing.Point(162, 40)
+        Me.picTarning2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.picTarning2.Name = "picTarning2"
         Me.picTarning2.Size = New System.Drawing.Size(64, 64)
         Me.picTarning2.TabIndex = 1
@@ -61,7 +67,8 @@ Partial Class Form1
         '
         'picTarning3
         '
-        Me.picTarning3.Location = New System.Drawing.Point(277, 53)
+        Me.picTarning3.Location = New System.Drawing.Point(242, 40)
+        Me.picTarning3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.picTarning3.Name = "picTarning3"
         Me.picTarning3.Size = New System.Drawing.Size(64, 64)
         Me.picTarning3.TabIndex = 1
@@ -79,17 +86,33 @@ Partial Class Form1
         Me.imlTarningar.Images.SetKeyName(4, "5.png")
         Me.imlTarningar.Images.SetKeyName(5, "6.png")
         '
-        'Form1
+        'Timer1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.Timer1.Tag = "Timer1"
+        '
+        'Timer2
+        '
+        Me.Timer2.Tag = "Timer2"
+        '
+        'Timer3
+        '
+        Me.Timer3.Tag = "Timer3"
+        '
+        'frmTärningar
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(446, 276)
+        Me.ClientSize = New System.Drawing.Size(390, 207)
         Me.Controls.Add(Me.picTarning3)
         Me.Controls.Add(Me.picTarning2)
         Me.Controls.Add(Me.picTarning1)
         Me.Controls.Add(Me.btnSla)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmTärningar"
+        Me.Text = "Slå tärningar"
         CType(Me.picTarning1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.picTarning2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.picTarning3, System.ComponentModel.ISupportInitialize).EndInit()
@@ -102,4 +125,7 @@ Partial Class Form1
     Friend WithEvents picTarning2 As PictureBox
     Friend WithEvents picTarning3 As PictureBox
     Friend WithEvents imlTarningar As ImageList
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
 End Class
--- P5_Tärningar/P5_Tärningar/Form1.vb
+++ P5_Tärningar/P5_Tärningar/Form1.vb
@@ -1,18 +1,68 @@
-﻿Public Class Form1
-    Private Sub btnSla_Click(sender As Object, e As EventArgs) Handles btnSla.Click
-        ' Rulla 100 varv
-        For i = 0 To 100
-            Dim tal As Integer
-            tal = Int(Rnd() * 6)
-            picTarning1.Image = imlTarningar.Images(tal)
-            tal = Int(Rnd() * 6)
-            picTarning2.Image = imlTarningar.Images(tal)
-            tal = Int(Rnd() * 6)
-            picTarning3.Image = imlTarningar.Images(tal)
-            ' Lägg in en liten delay innan nästa varv
-            For j = 0 To 1000
-                Application.DoEvents()
-            Next
-        Next
+﻿Public Class frmTärningar
+    Dim ticks(3) As Integer
+    Dim timers(3) As Timer
+    Dim picBoxes(3) As PictureBox
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' Skapa en array med timers
+        timers(0) = Timer1
+        timers(1) = Timer2
+        timers(2) = Timer3
+
+        ' Skapa en array med pictureboxar
+        picBoxes(0) = picTarning1
+        picBoxes(1) = picTarning2
+        picBoxes(2) = picTarning3
+
     End Sub
+    Private Sub btnSla_Click(sender As Object, e As EventArgs) Handles btnSla.Click
+        ' Förhindra spammklick på knappen
+        btnSla.Enabled = False
+
+        ' Initiera timers och räkna ticks
+        For i = 0 To 2
+            ticks(i) = Int(Rnd() * 100) + 10
+            timers(i).Interval = 100
+            timers(i).Start()
+        Next
+
+        ' Rulla tills alla tärningar räknat ner till 0
+        While ticks(0) > 0 And ticks(1) > 0 And ticks(2) > 0
+            Application.DoEvents()
+        End While
+
+        btnSla.Enabled = True
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick, Timer2.Tick, Timer3.Tick
+        ' Hantera timer-ticks för alla timers
+
+        Dim tal, index As Integer
+        ' Hitta vilken timer det är som skickar eventet
+        Select Case sender.Tag
+            Case "Timer1"
+                index = 0
+            Case "Timer2"
+                index = 1
+            Case "Timer3"
+                index = 2
+        End Select
+
+        ' Minska tick
+        ticks(index) -= 1
+
+        ' Rita ut tärningen
+        tal = Int(Rnd() * 6)
+        picBoxes(index).Image = imlTarningar.Images(tal)
+
+        ' Justera tick intervallet så att tärningen saktar ner
+        timers(index).Interval = 100 - ticks(index)
+
+        ' Stäng av timern om den är klar
+        If ticks(index) < 0 Then
+            timers(index).Enabled = False
+        End If
+
+    End Sub
+
 End Class
