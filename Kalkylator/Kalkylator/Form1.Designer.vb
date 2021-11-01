f4651507f1d4ede16267ba001e51cbfae9432135
Kjell Hansen (S1-KJHA-9205) <kjell.hansen@gymnasium.ax>
2021-11-01 15:29:29 +0200

    Skapat gränssnittet för nödvändiga funktioner

---
+++ Kalkylator/Kalkylator/Form1.Designer.vb
@@ -0,0 +1,287 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmKalkylator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnDecimal = New System.Windows.Forms.Button()
+        Me.btnEquals = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnAC = New System.Windows.Forms.Button()
+        Me.btnC = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn7
+        '
+        Me.btn7.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn7.Location = New System.Drawing.Point(19, 120)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(41, 35)
+        Me.btn7.TabIndex = 0
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn8.Location = New System.Drawing.Point(76, 120)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(41, 35)
+        Me.btn8.TabIndex = 0
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn9.Location = New System.Drawing.Point(133, 120)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(41, 35)
+        Me.btn9.TabIndex = 0
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btnDivide
+        '
+        Me.btnDivide.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnDivide.Location = New System.Drawing.Point(190, 120)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(41, 35)
+        Me.btnDivide.TabIndex = 0
+        Me.btnDivide.Text = "/"
+        Me.btnDivide.UseVisualStyleBackColor = True
+        '
+        'txtInput
+        '
+        Me.txtInput.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtInput.Location = New System.Drawing.Point(19, 12)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(213, 35)
+        Me.txtInput.TabIndex = 1
+        Me.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btn4
+        '
+        Me.btn4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn4.Location = New System.Drawing.Point(19, 174)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(41, 35)
+        Me.btn4.TabIndex = 0
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn5.Location = New System.Drawing.Point(76, 174)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(41, 35)
+        Me.btn5.TabIndex = 0
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn6.Location = New System.Drawing.Point(133, 174)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(41, 35)
+        Me.btn6.TabIndex = 0
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnMultiply.Location = New System.Drawing.Point(190, 174)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(41, 35)
+        Me.btnMultiply.TabIndex = 0
+        Me.btnMultiply.Text = "×"
+        Me.btnMultiply.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn1.Location = New System.Drawing.Point(19, 228)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(41, 35)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn2.Location = New System.Drawing.Point(76, 228)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(41, 35)
+        Me.btn2.TabIndex = 0
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn3.Location = New System.Drawing.Point(133, 228)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(41, 35)
+        Me.btn3.TabIndex = 0
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnSubtract.Location = New System.Drawing.Point(190, 228)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(41, 35)
+        Me.btnSubtract.TabIndex = 0
+        Me.btnSubtract.Text = "-"
+        Me.btnSubtract.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn0.Location = New System.Drawing.Point(19, 282)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(41, 35)
+        Me.btn0.TabIndex = 0
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btnDecimal
+        '
+        Me.btnDecimal.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnDecimal.Location = New System.Drawing.Point(76, 282)
+        Me.btnDecimal.Name = "btnDecimal"
+        Me.btnDecimal.Size = New System.Drawing.Size(41, 35)
+        Me.btnDecimal.TabIndex = 0
+        Me.btnDecimal.Text = "."
+        Me.btnDecimal.UseVisualStyleBackColor = True
+        '
+        'btnEquals
+        '
+        Me.btnEquals.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnEquals.Location = New System.Drawing.Point(133, 282)
+        Me.btnEquals.Name = "btnEquals"
+        Me.btnEquals.Size = New System.Drawing.Size(41, 35)
+        Me.btnEquals.TabIndex = 0
+        Me.btnEquals.Text = "="
+        Me.btnEquals.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnAdd.Location = New System.Drawing.Point(190, 282)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(41, 35)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnAC
+        '
+        Me.btnAC.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnAC.Location = New System.Drawing.Point(119, 66)
+        Me.btnAC.Name = "btnAC"
+        Me.btnAC.Size = New System.Drawing.Size(55, 35)
+        Me.btnAC.TabIndex = 0
+        Me.btnAC.Text = "AC"
+        Me.btnAC.UseVisualStyleBackColor = True
+        '
+        'btnC
+        '
+        Me.btnC.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnC.Location = New System.Drawing.Point(191, 66)
+        Me.btnC.Name = "btnC"
+        Me.btnC.Size = New System.Drawing.Size(41, 35)
+        Me.btnC.TabIndex = 0
+        Me.btnC.Text = "C"
+        Me.btnC.UseVisualStyleBackColor = True
+        '
+        'frmKalkylator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(252, 341)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnEquals)
+        Me.Controls.Add(Me.btnSubtract)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnDecimal)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btnC)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btnAC)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmKalkylator"
+        Me.Text = "Kalkylator, enkel"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnDecimal As Button
+    Friend WithEvents btnEquals As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnAC As Button
+    Friend WithEvents btnC As Button
+End Class
---
+++ Kalkylator/Kalkylator/Form1.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmKalkylator
+
+End Class
