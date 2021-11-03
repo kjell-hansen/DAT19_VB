12b6770528fcca8a8c67fa152442ea90e5850917
Kjell Hansen (S1-KJHA-9205) <kjell.hansen@gymnasium.ax>
2021-11-03 13:39:29 +0200

    Första beräkningarna funkar

---
+++ SimpelKalkylator/Form1.Designer.vb
@@ -0,0 +1,331 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txtOp = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnAddition = New System.Windows.Forms.Button()
+        Me.btnMultiplikation = New System.Windows.Forms.Button()
+        Me.btnDivision = New System.Windows.Forms.Button()
+        Me.btnSubtraktion = New System.Windows.Forms.Button()
+        Me.btnLikaMed = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnPunkt = New System.Windows.Forms.Button()
+        Me.btnAC = New System.Windows.Forms.Button()
+        Me.btnC = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(34, 31)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(125, 27)
+        Me.txt1.TabIndex = 0
+        '
+        'txtOp
+        '
+        Me.txtOp.Location = New System.Drawing.Point(200, 31)
+        Me.txtOp.Name = "txtOp"
+        Me.txtOp.Size = New System.Drawing.Size(125, 27)
+        Me.txtOp.TabIndex = 1
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(373, 31)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(125, 27)
+        Me.txt2.TabIndex = 2
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(34, 90)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(198, 27)
+        Me.txtInput.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Tal1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(200, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "operator"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(373, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Tal2"
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(82, 224)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(94, 29)
+        Me.btn1.TabIndex = 5
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(200, 224)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(94, 29)
+        Me.btn2.TabIndex = 6
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(314, 224)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(94, 29)
+        Me.btn3.TabIndex = 7
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(82, 189)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(94, 29)
+        Me.btn4.TabIndex = 8
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(200, 189)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(94, 29)
+        Me.btn5.TabIndex = 9
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(314, 189)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(94, 29)
+        Me.btn6.TabIndex = 10
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(82, 154)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(94, 29)
+        Me.btn7.TabIndex = 11
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(200, 154)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(94, 29)
+        Me.btn8.TabIndex = 12
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(314, 154)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(94, 29)
+        Me.btn9.TabIndex = 13
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btnAddition
+        '
+        Me.btnAddition.Location = New System.Drawing.Point(465, 224)
+        Me.btnAddition.Name = "btnAddition"
+        Me.btnAddition.Size = New System.Drawing.Size(94, 29)
+        Me.btnAddition.TabIndex = 14
+        Me.btnAddition.Text = "+"
+        Me.btnAddition.UseVisualStyleBackColor = True
+        '
+        'btnMultiplikation
+        '
+        Me.btnMultiplikation.Location = New System.Drawing.Point(465, 189)
+        Me.btnMultiplikation.Name = "btnMultiplikation"
+        Me.btnMultiplikation.Size = New System.Drawing.Size(94, 29)
+        Me.btnMultiplikation.TabIndex = 15
+        Me.btnMultiplikation.Text = "*"
+        Me.btnMultiplikation.UseVisualStyleBackColor = True
+        '
+        'btnDivision
+        '
+        Me.btnDivision.Location = New System.Drawing.Point(465, 154)
+        Me.btnDivision.Name = "btnDivision"
+        Me.btnDivision.Size = New System.Drawing.Size(94, 29)
+        Me.btnDivision.TabIndex = 16
+        Me.btnDivision.Text = "/"
+        Me.btnDivision.UseVisualStyleBackColor = True
+        '
+        'btnSubtraktion
+        '
+        Me.btnSubtraktion.Location = New System.Drawing.Point(465, 259)
+        Me.btnSubtraktion.Name = "btnSubtraktion"
+        Me.btnSubtraktion.Size = New System.Drawing.Size(94, 29)
+        Me.btnSubtraktion.TabIndex = 17
+        Me.btnSubtraktion.Text = "-"
+        Me.btnSubtraktion.UseVisualStyleBackColor = True
+        '
+        'btnLikaMed
+        '
+        Me.btnLikaMed.Location = New System.Drawing.Point(314, 259)
+        Me.btnLikaMed.Name = "btnLikaMed"
+        Me.btnLikaMed.Size = New System.Drawing.Size(94, 29)
+        Me.btnLikaMed.TabIndex = 18
+        Me.btnLikaMed.Text = "="
+        Me.btnLikaMed.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Location = New System.Drawing.Point(82, 259)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(94, 29)
+        Me.btn0.TabIndex = 19
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btnPunkt
+        '
+        Me.btnPunkt.Location = New System.Drawing.Point(200, 259)
+        Me.btnPunkt.Name = "btnPunkt"
+        Me.btnPunkt.Size = New System.Drawing.Size(94, 29)
+        Me.btnPunkt.TabIndex = 20
+        Me.btnPunkt.Text = "."
+        Me.btnPunkt.UseVisualStyleBackColor = True
+        '
+        'btnAC
+        '
+        Me.btnAC.Location = New System.Drawing.Point(314, 119)
+        Me.btnAC.Name = "btnAC"
+        Me.btnAC.Size = New System.Drawing.Size(94, 29)
+        Me.btnAC.TabIndex = 21
+        Me.btnAC.Text = "AC"
+        Me.btnAC.UseVisualStyleBackColor = True
+        '
+        'btnC
+        '
+        Me.btnC.Location = New System.Drawing.Point(465, 119)
+        Me.btnC.Name = "btnC"
+        Me.btnC.Size = New System.Drawing.Size(94, 29)
+        Me.btnC.TabIndex = 22
+        Me.btnC.Text = "C"
+        Me.btnC.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(598, 313)
+        Me.Controls.Add(Me.btnC)
+        Me.Controls.Add(Me.btnAC)
+        Me.Controls.Add(Me.btnPunkt)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnLikaMed)
+        Me.Controls.Add(Me.btnSubtraktion)
+        Me.Controls.Add(Me.btnDivision)
+        Me.Controls.Add(Me.btnMultiplikation)
+        Me.Controls.Add(Me.btnAddition)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txtOp)
+        Me.Controls.Add(Me.txt1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents txtOp As TextBox
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnAddition As Button
+    Friend WithEvents btnMultiplikation As Button
+    Friend WithEvents btnDivision As Button
+    Friend WithEvents btnSubtraktion As Button
+    Friend WithEvents btnLikaMed As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnPunkt As Button
+    Friend WithEvents btnAC As Button
+    Friend WithEvents btnC As Button
+End Class
---
+++ SimpelKalkylator/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    Dim nyBerakning As Boolean
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtInput.Text = "0"
+        nyBerakning = True
+    End Sub
+    Private Sub nummer_Click(sender As Object, e As EventArgs) Handles btn9.Click, btn8.Click, btn7.Click, btn6.Click, btn5.Click, btn4.Click, btn3.Click, btn2.Click, btn1.Click, btn0.Click
+        Dim maxLangd = 8 - txtInput.Text.Contains(".")
+        If nyBerakning Then
+            txtInput.Text = 0
+        End If
+
+        If txtInput.TextLength < maxLangd Then
+            If txtInput.Text = "0" Then
+                txtInput.Text = sender.text
+            Else
+                txtInput.Text &= sender.text
+            End If
+        End If
+        nyBerakning = False
+    End Sub
+
+    Private Sub btnPunkt_Click(sender As Object, e As EventArgs) Handles btnPunkt.Click
+        If txtInput.TextLength < 8 Then
+            txtInput.Text &= "."
+            btnPunkt.Enabled = False
+            nyBerakning = False
+        End If
+    End Sub
+
+    Private Sub btnAC_Click(sender As Object, e As EventArgs) Handles btnAC.Click
+        btnPunkt.Enabled = True
+        txtInput.Text = "0"
+        txt1.Text = ""
+        txtOp.Text = ""
+        txt2.Text = ""
+        nyBerakning = True
+    End Sub
+
+    Private Sub rakna_Click(sender As Object, e As EventArgs) Handles btnAddition.Click, btnSubtraktion.Click, btnMultiplikation.Click, btnLikaMed.Click, btnDivision.Click
+        txt2.Text = txtInput.Text
+
+        Select Case txtOp.Text
+            Case "+"
+                txtInput.Text = Val(txt1.Text) + Val(txt2.Text)
+            Case "-"
+                txtInput.Text = Val(txt1.Text) - Val(txt2.Text)
+            Case "/"
+                txtInput.Text = Val(txt1.Text) / Val(txt2.Text)
+            Case "*"
+                txtInput.Text = Val(txt1.Text) * Val(txt2.Text)
+            Case Else
+
+        End Select
+        txtOp.Text = sender.text
+        txt1.Text = txtInput.Text
+        nyBerakning = True
+    End Sub
+End Class
